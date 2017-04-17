b4456a27d3c234d7b005eb24e01e33d1e73660e5
v-kazaki <v-kazaki@microsoft.com>
2017-04-16 18:23:38 -0700

    14

--- infinite.bat
+++ infinite.bat
@@ -1,12 +1,20 @@
 @echo off
 SETLOCAL EnableDelayedExpansion
 
+set periodArg=%1
+
+if "%1"=="" (
+  SET periodArg=15 
+  )
+
+echo Period is %periodArg%
+
 :loop
 
 echo !date! !time!
 
 call node_modules\.bin\gulp run
 
-timeout /t 10 /nobreak > NUL
+timeout /t %periodArg% /nobreak > NUL
 
 goto loop
